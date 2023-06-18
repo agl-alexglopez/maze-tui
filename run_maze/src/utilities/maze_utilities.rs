7748ed1ab19b74028880638a4b59af18d19f7120
Alex Lopez <agl.alexglopez@gmail.com>
2023-06-18 15:09:09 -0700

    finalize helper functions

--- run_maze/src/utilities/maze_utilities.rs
+++ run_maze/src/utilities/maze_utilities.rs
@@ -33,7 +33,112 @@ pub fn add_positive_slope(maze: &mut maze::Maze, p: maze::Point) {
     let slope = (2.0f32 - row_size) / (2.0f32 - col_size);
     let b = 2.0f32 - (2.0f32 * slope);
     let on_slope = ((cur_row - b) / slope) as i32;
-    if p.col == on_slope && p.col < maze.col_size() - 2 && p.col > 1 {}
+    if p.col == on_slope && p.col < maze.col_size() - 2 && p.col > 1 {
+        build_path(maze, p);
+        if p.col + 1 < maze.col_size() - 2 {
+            build_path(maze, maze::Point {row: p.row, col: p.col + 1});
+        }
+        if p.col - 1 > 1 {
+            build_path(maze, maze::Point {row: p.row, col: p.col - 1});
+        }
+        if p.col + 2 < maze.col_size() - 2 {
+            build_path(maze, maze::Point {row: p.row, col: p.col + 2});
+        }
+        if p.col - 2 > 1 {
+            build_path(maze, maze::Point {row: p.row, col: p.col - 2});
+        }
+    }
+}
+
+pub fn add_positive_slope_animated(maze: &mut maze::Maze, p: maze::Point, speed: SpeedUnit) {
+    let row_size = maze.row_size() as f32 - 2.0f32;
+    let col_size = maze.col_size() as f32 - 2.0f32;
+    let cur_row = p.row as f32;
+    let slope = (2.0f32 - row_size) / (2.0f32 - col_size);
+    let b = 2.0f32 - (2.0f32 * slope);
+    let on_slope = ((cur_row - b) / slope) as i32;
+    if p.col == on_slope && p.col < maze.col_size() - 2 && p.col > 1 {
+        build_path_animated(maze, p, speed);
+        if p.col + 1 < maze.col_size() - 2 {
+            build_path_animated(maze, maze::Point {row: p.row, col: p.col + 1}, speed);
+        }
+        if p.col - 1 > 1 {
+            build_path_animated(maze, maze::Point {row: p.row, col: p.col - 1}, speed);
+        }
+        if p.col + 2 < maze.col_size() - 2 {
+            build_path_animated(maze, maze::Point {row: p.row, col: p.col + 2}, speed);
+        }
+        if p.col - 2 > 1 {
+            build_path_animated(maze, maze::Point {row: p.row, col: p.col - 2}, speed);
+        }
+    }
+}
+
+pub fn add_negative_slope(maze: &mut maze::Maze, p: maze::Point) {
+    let row_size = maze.row_size() as f32 - 2.0f32;
+    let col_size = maze.col_size() as f32 - 2.0f32;
+    let cur_row = p.row as f32;
+    let slope = (2.0f32 - row_size) / (col_size - 2.0f32);
+    let b = row_size - (2.0f32 * slope);
+    let on_line = ((cur_row - b) / slope) as i32;
+    if p.col == on_line && p.col > 1 && p.col < maze.col_size() - 2 && p.row < maze.row_size() - 2 {
+        build_path(maze, p);
+        if p.col + 1 < maze.col_size() - 2 {
+            build_path(maze, maze::Point {row: p.row, col: p.col + 1});
+        }
+        if p.col - 1 > 1 {
+            build_path(maze, maze::Point {row: p.row, col: p.col - 1});
+        }
+        if p.col + 2 < maze.col_size() - 2 {
+            build_path(maze, maze::Point {row: p.row, col: p.col + 2});
+        }
+        if p.col - 2 > 1 {
+            build_path(maze, maze::Point {row: p.row, col: p.col - 2});
+        }
+    }
+}
+
+pub fn add_negative_slope_animated(maze: &mut maze::Maze, p: maze::Point, speed: SpeedUnit) {
+    let row_size = maze.row_size() as f32 - 2.0f32;
+    let col_size = maze.col_size() as f32 - 2.0f32;
+    let cur_row = p.row as f32;
+    let slope = (2.0f32 - row_size) / (col_size - 2.0f32);
+    let b = row_size - (2.0f32 * slope);
+    let on_line = ((cur_row - b) / slope) as i32;
+    if p.col == on_line && p.col > 1 && p.col < maze.col_size() - 2 && p.row < maze.row_size() - 2 {
+        build_path_animated(maze, p, speed);
+        if p.col + 1 < maze.col_size() - 2 {
+            build_path_animated(maze, maze::Point {row: p.row, col: p.col + 1}, speed);
+        }
+        if p.col - 1 > 1 {
+            build_path_animated(maze, maze::Point {row: p.row, col: p.col - 1}, speed);
+        }
+        if p.col + 2 < maze.col_size() - 2 {
+            build_path_animated(maze, maze::Point {row: p.row, col: p.col + 2}, speed);
+        }
+        if p.col - 2 > 1 {
+            build_path_animated(maze, maze::Point {row: p.row, col: p.col - 2}, speed);
+        }
+    }
+}
+
+pub fn add_x(maze: &mut maze::Maze) {
+    for r in 1..maze.row_size() - 1 {
+        for c in 1..maze.col_size() - 1 {
+            add_positive_slope(maze, maze::Point {row: r, col: c});
+            add_negative_slope(maze, maze::Point {row: r, col: c});
+        }
+    }
+}
+
+pub fn add_x_animated(maze: &mut maze::Maze, speed: BuilderSpeed) {
+    let animation: SpeedUnit = BUILDER_SPEEDS[speed as usize];
+    for r in 1..maze.row_size() - 1 {
+        for c in 1..maze.col_size() - 1 {
+            add_positive_slope_animated(maze, maze::Point {row: r, col: c}, animation);
+            add_negative_slope_animated(maze, maze::Point {row: r, col: c}, animation);
+        }
+    }
 }
 
 pub fn build_wall_outline(maze: &mut maze::Maze) {
