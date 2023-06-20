9dd3d19c9d3f72e11592ddcac201f9725e82a4df
Alex Lopez <agl.alexglopez@gmail.com>
2023-06-19 23:59:57 -0700

    forming multithreading structures

--- run_maze/src/solvers/dfs.rs
+++ run_maze/src/solvers/dfs.rs
@@ -1,6 +1,25 @@
+use crate::maze;
 use crate::utilities::print_util;
+use crate::utilities::solve_util;
 
 use std::{thread, time};
-use std::sync::Mutex;
+use std::sync::{Arc, Mutex};
 
-// Thread construct only needs to occur within scope of function multithreading, no mutex backpack.
+struct SolverMonitor<'a> {
+    maze_ref: &'a mut maze::Maze,
+    winning_index: Option<usize>
+}
+
+impl<'a> SolverMonitor<'a> {
+    fn new(maze: &'a mut maze::Maze) -> Self {
+        Self {maze_ref: maze, winning_index: None}
+    }
+}
+
+struct SolverPack<'a> {
+    monitor: &'a mut Arc<Mutex<SolverMonitor<'a>>>,
+    speed: Option<solve_util::SolverSpeed>,
+    starts: Vec<maze::Point>,
+    thread_paths: Vec<maze::Point>
+}
+
