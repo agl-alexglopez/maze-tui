abe6bb53ee51428307cf2eed4ca4bbd1e0b67f25
Alex Lopez <agl.alexglopez@gmail.com>
2023-06-19 21:18:42 -0700

    starting on threading soon

--- run_maze/src/solvers/dfs.rs
+++ run_maze/src/solvers/dfs.rs
@@ -1,1 +1,6 @@
+use crate::utilities::print_util;
 
+use std::{thread, time};
+use std::sync::Mutex;
+
+// Thread construct only needs to occur within scope of function multithreading, no mutex backpack.
