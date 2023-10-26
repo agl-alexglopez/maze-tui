179a100b12479e9eeaf0b411f18278fc03b1d7b1
Alex Lopez <agl.alexglopez@gmail.com>
2023-10-26 11:32:04 -0700

    update info prompt border

--- maze_progs/run_tui/src/tui.rs
+++ maze_progs/run_tui/src/tui.rs
@@ -420,6 +420,7 @@ fn ui_info_prompt(f: &mut Frame<'_>) {
         .block(
             Block::default()
                 .borders(Borders::ALL)
+                .border_type(BorderType::Double)
                 .border_style(Style::new().fg(Color::Yellow))
                 .style(Style::default().bg(Color::Black)),
         )
