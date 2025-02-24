928766dcb6b922e6049e6de6d0077536aa085209
Mark English <thatmarkenglishguy@gmail.com>
2025-02-24 14:15:38 +0000

    clippy Added BuildHistoryType to avoid comparing function pointers.

--- maze_tui/tables/src/lib.rs
+++ maze_tui/tables/src/lib.rs
@@ -36,7 +36,7 @@ pub enum ViewingMode {
 #[derive(Clone, Copy)]
 pub struct HistoryRunner {
     pub args: maze::MazeArgs,
-    pub build: BuildHistoryFunction,
+    pub build: BuildHistoryType,
     pub modify: Option<BuildHistoryFunction>,
     pub solve: SolveHistoryFunction,
 }
@@ -50,7 +50,7 @@ impl HistoryRunner {
                 offset: maze::Offset::default(),
                 style: maze::MazeStyle::Sharp,
             },
-            build: recursive_backtracker::generate_history,
+            build: BuildHistoryType::RecursiveBacktracker(recursive_backtracker::generate_history),
             modify: None,
             solve: dfs::hunt_history,
         }
@@ -73,7 +73,7 @@ where
         .map(|(_, t)| t.clone())
 }
 
-pub fn load_info(cur_builder: &BuildHistoryFunction) -> &'static str {
+pub fn load_info(cur_builder: &BuildHistoryType) -> &'static str {
     match DESCRIPTIONS.iter().find(|(func, _)| func == cur_builder) {
         Some(&(_, desc)) => desc,
         None => "Coming Soon!",
@@ -100,20 +100,69 @@ pub const WALL_STYLES: [(&str, maze::MazeStyle); 8] = [
     ("spikes", maze::MazeStyle::Spikes),
 ];
 
+#[derive(Clone)]
+pub struct BuildHistoryEntry(pub BuildHistoryFunction);
+#[derive(Clone, Copy, PartialEq, Eq)]
+pub enum BuildHistoryType {
+    Arena(BuildHistoryFunction),
+    RecursiveBacktracker(BuildHistoryFunction),
+    HuntKill(BuildHistoryFunction),
+    RecursiveSubdivision(BuildHistoryFunction),
+    Prim(BuildHistoryFunction),
+    Kruskal(BuildHistoryFunction),
+    Eller(BuildHistoryFunction),
+    WilsonCarver(BuildHistoryFunction),
+    WilsonAdder(BuildHistoryFunction),
+    Grid(BuildHistoryFunction),
+}
+impl BuildHistoryType {
+    pub fn function(&self) -> fn(std::sync::Arc<std::sync::Mutex<monitor::Monitor>>) {
+        match self {
+            BuildHistoryType::Arena(f) => *f,
+            BuildHistoryType::RecursiveBacktracker(f) => *f,
+            BuildHistoryType::HuntKill(f) => *f,
+            BuildHistoryType::RecursiveSubdivision(f) => *f,
+            BuildHistoryType::Prim(f) => *f,
+            BuildHistoryType::Kruskal(f) => *f,
+            BuildHistoryType::Eller(f) => *f,
+            BuildHistoryType::WilsonCarver(f) => *f,
+            BuildHistoryType::WilsonAdder(f) => *f,
+            BuildHistoryType::Grid(f) => *f,
+        }
+    }
+}
 ///
 /// History and playback specific tables
 ///
-pub const HISTORY_BUILDERS: [(&str, BuildHistoryFunction); 10] = [
-    ("arena", arena::generate_history),
-    ("rdfs", recursive_backtracker::generate_history),
-    ("hunt-kill", hunt_kill::generate_history),
-    ("fractal", recursive_subdivision::generate_history),
-    ("prim", prim::generate_history),
-    ("kruskal", kruskal::generate_history),
-    ("eller", eller::generate_history),
-    ("wilson", wilson_carver::generate_history),
-    ("wilson-walls", wilson_adder::generate_history),
-    ("grid", grid::generate_history),
+pub const HISTORY_BUILDERS: [(&str, BuildHistoryType); 10] = [
+    ("arena", BuildHistoryType::Arena(arena::generate_history)),
+    (
+        "rdfs",
+        BuildHistoryType::RecursiveBacktracker(recursive_backtracker::generate_history),
+    ),
+    (
+        "hunt-kill",
+        BuildHistoryType::HuntKill(hunt_kill::generate_history),
+    ),
+    (
+        "fractal",
+        BuildHistoryType::RecursiveSubdivision(recursive_subdivision::generate_history),
+    ),
+    ("prim", BuildHistoryType::Prim(prim::generate_history)),
+    (
+        "kruskal",
+        BuildHistoryType::Kruskal(kruskal::generate_history),
+    ),
+    ("eller", BuildHistoryType::Eller(eller::generate_history)),
+    (
+        "wilson",
+        BuildHistoryType::WilsonCarver(wilson_carver::generate_history),
+    ),
+    (
+        "wilson-walls",
+        BuildHistoryType::WilsonAdder(wilson_adder::generate_history),
+    ),
+    ("grid", BuildHistoryType::Grid(grid::generate_history)),
 ];
 
 pub const HISTORY_MODIFICATIONS: [(&str, BuildHistoryFunction); 2] = [
@@ -138,45 +187,45 @@ pub const HISTORY_SOLVERS: [(&str, SolveHistoryFunction); 14] = [
     ("runs", runs::paint_run_lengths_history),
 ];
 
-pub static DESCRIPTIONS: [(BuildHistoryFunction, &str); 10] = [
+pub static DESCRIPTIONS: [(BuildHistoryType, &str); 10] = [
     (
-        builders::arena::generate_history,
+        BuildHistoryType::Arena(builders::arena::generate_history),
         include_str!("../../res/arena.txt"),
     ),
     (
-        builders::eller::generate_history,
+        BuildHistoryType::Eller(builders::eller::generate_history),
         include_str!("../../res/eller.txt"),
     ),
     (
-        builders::grid::generate_history,
+        BuildHistoryType::Grid(builders::grid::generate_history),
         include_str!("../../res/grid.txt"),
     ),
     (
-        builders::hunt_kill::generate_history,
+        BuildHistoryType::HuntKill(builders::hunt_kill::generate_history),
         include_str!("../../res/hunt_kill.txt"),
     ),
     (
-        builders::kruskal::generate_history,
+        BuildHistoryType::Kruskal(builders::kruskal::generate_history),
         include_str!("../../res/kruskal.txt"),
     ),
     (
-        builders::prim::generate_history,
+        BuildHistoryType::Prim(builders::prim::generate_history),
         include_str!("../../res/prim.txt"),
     ),
     (
-        builders::recursive_backtracker::generate_history,
+        BuildHistoryType::RecursiveBacktracker(builders::recursive_backtracker::generate_history),
         include_str!("../../res/recursive_backtracker.txt"),
     ),
     (
-        builders::recursive_subdivision::generate_history,
+        BuildHistoryType::RecursiveSubdivision(builders::recursive_subdivision::generate_history),
         include_str!("../../res/recursive_subdivision.txt"),
     ),
     (
-        builders::wilson_adder::generate_history,
+        BuildHistoryType::WilsonAdder(builders::wilson_adder::generate_history),
         include_str!("../../res/wilson_adder.txt"),
     ),
     (
-        builders::wilson_carver::generate_history,
+        BuildHistoryType::WilsonCarver(builders::wilson_carver::generate_history),
         include_str!("../../res/wilson_carver.txt"),
     ),
 ];
